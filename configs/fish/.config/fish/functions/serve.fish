ec82c10f4cf53c7daeea7f1ff45c01c93c64cc36
Yash Agarwal <13733968+yashhere@users.noreply.github.com>
2025-01-28 12:41:40 +0530

    don't open browser on http serve

--- configs/fish/.config/fish/functions/serve.fish
+++ configs/fish/.config/fish/functions/serve.fish
@@ -1,9 +1,7 @@
-function serve --description 'Start a local server for the current directory, open in browser'
+function serve --description 'Start a local server for the current directory'
 	set -l port 8080
 	if test (count $argv) -eq 1
 		set port $argv[1]
 	end
-	python3 -m http.server $port &
-	open http://localhost:$port
-	fg
+	python3 -m http.server $port
 end
